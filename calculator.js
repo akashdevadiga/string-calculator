e84754c7bb202dd502dd6e22d925d27a50188f4d
akashdevadiga <akashdevadiga260@gmail.com>
2024-08-11 22:44:28 +0530

    test case 3 : new line

--- calculator.js
+++ calculator.js
@@ -1,20 +1,22 @@
 function add(data) {
-    const str = data.trim();
+    let str = data.trim();
 
     if (!str) {
         return 0;
     }
 
+    str = str.replace(/\n/g, ',');
+
     if (!str.includes(",")) {
         return parseInt(str);
     }
 
-    const numArr = str.split(",").map((val) => parseInt(val));
+    const resArr = str.split(",").map((val) => parseInt(val));
 
-    return numArr.reduce((acc, num) => acc + num, 0);
+    return resArr.reduce((acc, num) => acc + num, 0);
 }
 
-console.log("res: ", add("1,2,3,4"));
+console.log("res: ", add("1\n2 \n3, 4"));
 
 module.exports = {
     add
--- calculator.test.js
+++ calculator.test.js
@@ -15,3 +15,7 @@ test('two numbers separated by a comma return their sum', () => {
 test('multiple numbers separated by a comma return their sum', () => {
     expect(add("1,2,3,4")).toBe(10);
 });
+
+test('numbers with new lines or commas returns sum', () => {
+    expect(add("1\n2,3")).toBe(6);
+});
